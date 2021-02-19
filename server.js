b891e5d2c2c958d73dfbb0f6fe36a746230465ae
JGG <javieron.garcia@gmail.com>
2021-02-19 13:29:20 +0100

    mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs"); // calling ejs
@@ -11,6 +12,87 @@ app.use(
     extended: true,
   })
 ); // using bodyparser
+
+// Mongoose
+mongoose.connect("mongodb://localhost:27017/hvpInventory", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const productSchema = new mongoose.Schema({
+  product_name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  urban_storage_stock: Number,
+  urban_counter_stock: Number,
+  urban_counter_stock_min: Number,
+  urban_counter_stock_rec: Number,
+  urban_stock_min: Number,
+  urban_stock_rec: Number,
+  harbor_storage_stock: Number,
+  harbor_counter_stock: Number,
+  harbor_counter_stock_min: Number,
+  harbor_counter_stock_rec: Number,
+  harbor_stock_min: Number,
+  harbor_stock_rec: Number,
+  montejo_stock: Number,
+  montejo_stock_min: Number,
+  montejo_stock_rec: Number,
+});
+
+// Model
+// the first parameter (Product) is the individual of the collection (products)
+// the second parameter is the schema
+const Product = mongoose.model("Product", productSchema);
+
+// The object
+const product = new Product({
+  product_name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit",
+});
+
+product.save();
+
+// 336 save the object
+// fruit.save();
+
+////////////////// 337 Reading
+Product.find((err, products) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(products);
+  }
+});
+
+// 339 update and delete
+Product.updateOne(
+  {
+    _id: "5ff65bb46405dc31acce533b",
+  },
+  {
+    name: "Peach",
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("succesfully updated");
+    }
+  }
+);
+
+// 339 delete
+Product.deleteOne(
+  {
+    name: "Peach",
+  },
+  (err) => {
+    //
+  }
+);
 
 // app.get("/", (req, res) => res.send("Hello"));
 // to render ejs
